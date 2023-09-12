4192530ca7b0225ca2a44a76e407cdf57455cec3
Elhanan Avihail <etsAvihail@gmail.com>
2023-09-12 21:33:35 +0300

    js change

--- js/main.js
+++ js/main.js
@@ -2,5 +2,12 @@ console.log('Hi from main js')
 
 // Using the GUI - publish project
 // Show invitation process + clone from VSC
-// show branches
+// show branches.
+// 
+// motivation for team's work:
+/* 
+a. less conflict
+b. more task management order 
+c. תהליך אישור - PR
+*/
 
