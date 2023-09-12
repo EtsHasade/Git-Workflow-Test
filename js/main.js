2eb8c344f86a7dea9e8a41301ba549fa7be12724
Elhanan Avihail <etsAvihail@gmail.com>
2023-09-12 21:57:38 +0300

    chanhe popo

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ console.log('Hi from main js')
 // Using the GUI - publish project
 // Show invitation process + clone from VSC
 // show branches.
-// 
+// -
 // motivation for team's work:
 /* 
 a. less conflict
